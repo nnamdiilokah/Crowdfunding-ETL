3ad413927c8cd350b13be0830de6cce46dc74050
Nnamdi Ilokah <nnamdi.ilokah@mail.utoronto.ca>
2022-10-16 14:33:09 -0400

    adding remaining goal amount for live campaign

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -1,23 +1,30 @@
 -- Challenge Bonus queries.
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
-
+SELECT cf_id, backers_count FROM campaign
+WHERE outcome = 'live'
+ORDER BY backers_count DESC
 
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
-
-
+SELECT cf_id, COUNT(backer_id) FROM backers
+GROUP BY cf_id
+ORDER BY COUNT(backer_id) DESC
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-
-
-
+SELECT c.first_name, c.last_name, c.email, (ca.goal - ca.pledged) as Remaining_Goal_Amount
+INTO email_contacts_remaining_goal_amount
+FROM contacts as c
+INNER JOIN campaign as ca
+ON c.contact_id = ca.contact_id
+WHERE ca.outcome = 'live'
+ORDER BY (ca.goal - ca.pledged) DESC;
 
 -- Check the table
-
+SELECT * FROM email_contacts_remaining_goal_amount;
 
 -- 4. (5 pts)
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
