9e79f05e7dcc0b952a2bc47f1d04f86cbaa66e5c
Nnamdi Ilokah <nnamdi.ilokah@mail.utoronto.ca>
2022-10-18 13:03:30 -0400

    adding email_backers_remaining_goal_amount query and csv file

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -3,14 +3,13 @@
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
 SELECT cf_id, backers_count FROM campaign
 WHERE outcome = 'live'
-ORDER BY backers_count DESC
-
+ORDER BY backers_count DESC;
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
 SELECT cf_id, COUNT(backer_id) FROM backers
 GROUP BY cf_id
-ORDER BY COUNT(backer_id) DESC
+ORDER BY COUNT(backer_id) DESC;
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
@@ -30,9 +29,15 @@ SELECT * FROM email_contacts_remaining_goal_amount;
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
-
+SELECT b.email, b.first_name, b.last_name, b.cf_id,
+       ca.company_name, ca.description, ca.end_date, (ca.goal - ca.pledged) as Left_of_Goal
+INTO email_backers_remaining_goal_amount
+FROM backers as b
+INNER JOIN campaign as ca
+ON b.cf_id = ca.cf_id
+ORDER BY b.email DESC;
 
 
 -- Check the table
-
+SELECT * FROM email_backers_remaining_goal_amount;
 
